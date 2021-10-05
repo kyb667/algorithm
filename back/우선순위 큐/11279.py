1e31e4161c99ac6c163f2f30769b1f208dbd3b48
kyb667 <kyb667@naver.com>
2021-10-05 23:38:06 +0900

    최대 힙

---
+++ back/우선순위 큐/11279.py
@@ -0,0 +1,11 @@
+from sys import stdin
+import heapq
+
+n = int(stdin.readline().strip())
+que = []
+for _ in range(n):
+    x = int(stdin.readline().strip())
+    if x == 0:
+        print(heapq.heappop(que)[1] if que else 0)
+    else:
+        heapq.heappush(que, (-x, x))
